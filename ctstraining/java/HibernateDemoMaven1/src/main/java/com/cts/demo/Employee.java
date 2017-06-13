2a87396d2eefd0303a63557f5ad3ab15f32382f5
puneetvashisht <puneetvashisht@gmail.com>
2017-06-13 20:20:10 +0530

    day 1

---
+++ ctstraining/java/HibernateDemoMaven1/src/main/java/com/cts/demo/Employee.java
@@ -0,0 +1,44 @@
+package com.cts.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "EMP")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	int id;
+	@Column(name="NAME")
+	String name;
+	public Employee(){
+		
+	}
+	public Employee(String name) {
+		super();
+//		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
